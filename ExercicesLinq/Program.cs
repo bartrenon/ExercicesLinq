74e8d4a7ed3847651cf5d09300ee1637b33f2cda
Renon bart <b.renon@mail.com>
2026-03-25 15:22:40 +0100

    exo de 4 à 5

--- ExercicesLinq/Program.cs
+++ ExercicesLinq/Program.cs
@@ -56,17 +56,198 @@ internal class Program
 
         //3.1
 
-        var result = dc.Students.Where(s => s.BirthDate.Year < 1955).
+        //var result = dc.Students.Where(s => s.BirthDate.Year < 1955).
+        //Select(s => new
+        //{
+        //    s.Last_Name,
+        //    s.Year_Result,
+        //    Statut = s.Year_Result >= 12 ? "OK" : "KO"
+        //});
+
+        //foreach (var s in result)
+        //{
+        //    Console.WriteLine($"{s.Last_Name} - {s.Year_Result} - {s.Statut}");
+        //}
+
+        //3.2
+
+        /*var result = dc.Students.Where(s => s.BirthDate.Year >= 1955 && s.BirthDate.Year <= 1965).
         Select(s => new
         {
             s.Last_Name,
             s.Year_Result,
-            Statut = s.Year_Result >= 12 ? "OK" : "KO"
+            Categorie = s.Year_Result < 10 ? "inférieure" :
+                s.Year_Result == 10 ? "neutre" :
+                    "supérieure"
         });
 
         foreach (var s in result)
         {
-            Console.WriteLine($"{s.Last_Name} - {s.Year_Result} - {s.Statut}");
+            Console.WriteLine($"{s.Last_Name} - {s.Year_Result} - {s.Categorie}");
+        }*/
+
+        //3.3
+
+        //var result = dc.Students.Where(s => s.Last_Name.EndsWith("r")).
+        //Select(s => new
+        //{
+        //    s.Last_Name,
+        //    s.Section_ID
+        //});
+
+        // foreach (var s in result)
+        // {
+        //     Console.WriteLine($"{s.Last_Name} - {s.Section_ID}");
+        // }
+
+        //3.4
+
+        //var result = dc.Students.Where(s => s.Year_Result <= 3).
+        //    OrderByDescending(s => s.Year_Result).
+        //Select(s => new
+        //{
+        //    s.Last_Name,
+        //    s.Year_Result
+        //});
+
+        //foreach (var s in result)
+        //{
+        //    Console.WriteLine($"{s.Last_Name} - {s.Year_Result}");
+        //}
+
+        //3.5
+
+        //var result = dc.Students.Where(s => s.Section_ID == 1110).
+        //    OrderBy(s => s.Last_Name).
+        //Select(s => new
+        //{
+        //    Name = $"{s.Last_Name} {s.First_Name}",
+        //    s.Year_Result
+        //});
+
+        //foreach (var s in result)
+        //{
+        //    Console.WriteLine($"{s.Name} - {s.Year_Result}");
+        //}
+
+        //3.6
+
+        //var result = dc.Students.Where(s => (s.Section_ID == 1010 || s.Section_ID == 1020) && (s.Year_Result < 12 || s.Year_Result > 18)).
+        //    OrderBy(s => s.Section_ID).
+        //Select(s => new
+        //{
+        //    s.Last_Name,
+        //    s.Section_ID,
+        //    s.Year_Result
+        //});
+
+        //foreach (var s in result)
+        //{
+        //    Console.WriteLine($"{s.Last_Name} - {s.Section_ID} - {s.Year_Result}");
+        //}
+
+        //3.7
+
+        //var result = dc.Students.Where(s => s.Section_ID.ToString().StartsWith("13")).
+        //Select(s => new
+        //{
+        //    s.Last_Name,
+        //    s.Section_ID,
+        //    result_100 = s.Year_Result * 5
+        //}).Where(s => s.result_100 <+ 60).OrderByDescending(s => s.result_100);
+
+        //foreach (var s in result)
+        //{
+        //    Console.WriteLine($"{s.Last_Name} - {s.Section_ID} - {s.result_100}");
+        //}
+
+        //4 COUNT MIN MAX SUM AVRAGE
+
+        // 4.1 
+
+        //var moyenne = dc.Students.Average(s => s.Year_Result);
+
+        //Console.WriteLine(moyenne);
+
+        // 4.2 
+
+        //var max = dc.Students.Max(s => s.Year_Result);
+        //Console.WriteLine(max);
+
+        // 4.3
+
+        //var sum = dc.Students.Sum(s => s.Year_Result);
+        //Console.WriteLine(sum);
+
+        // 4.4
+
+        //var min = dc.Students.Min(s => s.Year_Result);
+        //Console.WriteLine(min);
+
+        // 4.5
+
+        //var min = dc.Students.Where(s => s.Year_Result % 2 != 0).Count();
+        //Console.WriteLine(min);
+
+        // 5 groupeBy Join GroupJoin 
+
+        //5.1
+
+        //var result = dc.Students.GroupBy(s => s.Section_ID)
+        //   .Select(g => new 
+        //   {
+        //        Section = g.Key,
+        //        MaxResult = g.Max(s => s.Year_Result)
+        //    });
+
+        //foreach (var s in result)
+        //{
+        //    Console.WriteLine($"Section {s.Section} : Max = {s.MaxResult}");
+        //}
+
+        // 5.2
+
+        //var result = dc.Students.Where(s => s.Section_ID.ToString().StartsWith("10"))
+        //   .GroupBy(s => s.Section_ID)
+        //   .Select(g => new
+        //   {
+        //       Section = g.Key,
+        //       MoySection = g.Average(s => s.Year_Result)
+        //   });
+
+        //foreach (var s in result)
+        //{
+        //    Console.WriteLine($"Section {s.Section} : AVG = {s.MoySection}");
+        //}
+
+        // 5.3
+
+        //var result = dc.Students.Where(s => s.BirthDate.Year >= 1970 && s.BirthDate.Year <= 1985)
+        //   .GroupBy(s => s.BirthDate.Month)
+        //   .Select(g => new
+        //   {
+        //       BirthMonth = g.Key,
+        //       AVGResult = g.Average(s => s.Year_Result)
+        //   });
+
+        //foreach (var s in result)
+        //{
+        //    Console.WriteLine($"{s.BirthMonth} : AVG = {s.AVGResult}");
+        //}
+
+        // 5.4
+
+        var result = dc.Students.GroupBy(s => s.Section_ID)
+           .Where(s => s.Count() > 3)
+           .Select(g => new
+           {
+               Section = g.Key,
+               AVGResult = g.Average(s => s.Year_Result)
+           });
+
+        foreach (var s in result)
+        {
+            Console.WriteLine($"{s.Section} : AVG = {s.AVGResult}");
         }
     }
 }
